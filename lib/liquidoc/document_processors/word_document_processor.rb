9c0bd3a511744cd14715822b16c5b5ecd36867ec
jwettstein <jeff@leverage.law>
2023-12-12 15:03:39 -0800

    Update word_document_processor.rb

--- lib/liquidoc/document_processors/word_document_processor.rb
+++ lib/liquidoc/document_processors/word_document_processor.rb
@@ -13,7 +13,7 @@ module Liquidoc
           document_entries.each do |document_entry|
             out.put_next_entry document_entry.name
 
-            if document_entry.name.include?(".xml")
+            if document_entry.name.include?("header") or document_entry.name.include?("footer") or document_entry.name == 'word/document.xml'
               self.current_file = document_entry.name
               out.write template.render(context)
             else
