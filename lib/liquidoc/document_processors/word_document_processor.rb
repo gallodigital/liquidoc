eed827ba1ba44b0539d94909f03e574e773227d2
jwettstein <jeff@leverage.law>
2023-12-12 14:40:55 -0800

    Update word_document_processor.rb
    Enhance processor to apply to all .xml files so headers/footers and other files will be templated as well.

--- lib/liquidoc/document_processors/word_document_processor.rb
+++ lib/liquidoc/document_processors/word_document_processor.rb
@@ -6,13 +6,15 @@ module Liquidoc
       def_delegator :template, :errors
       def_delegator :template, :tags
       def_delegator :template, :variables
-
+      attr_accessor :current_file
+      
       def render(context = {})
         Zip::OutputStream.write_buffer(StringIO.new) do |out|
           document_entries.each do |document_entry|
             out.put_next_entry document_entry.name
 
-            if document_entry.name == CONTENT_ENTRY_NAME
+            if document_entry.include? == ".xml"
+              self.current_file = document_entry.name
               out.write template.render(context)
             else
               out.write document_entries.read(document_entry.name)
@@ -22,15 +24,15 @@ module Liquidoc
       end
 
       def content
-        @content ||= document_entries.read(CONTENT_ENTRY_NAME)
+        @content = document_entries.read(self.current_file)
       end
 
       def sanitizer
-        @sanitizer ||= Sanitizers::WordSanitizer.new(content)
+        @sanitizer = Sanitizers::WordSanitizer.new(content)
       end
 
       def template
-        @template ||= Template.new(sanitizer.document)
+        @template = Template.new(sanitizer.document)
       end
 
       def clean_content
